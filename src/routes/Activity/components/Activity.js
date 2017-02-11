7c43416736198bac32959a7c55fc1e652c6f9e69
auchiyam <auchiyam1994@gmail.com>
2017-02-11 12:13:43 -0600

    added temporary switch for each roles

--- src/routes/Activity/components/Activity.js
+++ src/routes/Activity/components/Activity.js
@@ -2,12 +2,43 @@ import React from 'react';
 import RequireAuth from 'components/Auth/RequireAuth.js';
 
 class Activity extends React.Component {
+    constructor(props)
+    {
+        super(props)
+        this.testState = { role: 'STUDENT' }
+    }
+
     render() {
-        return (
-            <div>
-                <h2>Activity</h2>
-            </div>
-        );
+        switch (this.testState.role) {
+            case 'STUDENT':
+                return (
+                    <div>
+                        <h2>This is the activity page for students</h2>
+                    </div>
+                );
+                break;
+            case 'COORDINATOR':
+                return (
+                    <div>
+                        <h2>This is the activity page for coordinators</h2>
+                    </div>
+                );
+                break;
+            case 'GUEST':
+                return (
+                    <div>
+                        <h2>Hide activity page for guests</h2>
+                    </div>
+                );
+                break;
+            default:
+                return (
+                    <div>
+                        <h2>Error, this page should not be loaded.  Invalid authstate</h2>
+                    </div>
+                );
+                break;
+        }
     }
 }
 
