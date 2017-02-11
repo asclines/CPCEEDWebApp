b04eb4ad869e2c093bc090b327b35db645ef0ff7
auchiyam <auchiyam1994@gmail.com>
2017-02-11 16:27:22 -0600

    minor formatting changes

--- src/routes/Activity/components/Activity.js
+++ src/routes/Activity/components/Activity.js
@@ -10,86 +10,78 @@ function ParseActivity(props) {
 	//currently simply returns the item as is because the array expected is a simple literal string
 	return (
 		<TableRow>
-			<td>pending</td>
+			<td>{props.status}</td>
 			<td>{props.activity}</td>
 		</TableRow>
 	);
 }
 
 //Generate the list of activities to view depending on the list provided
-//todo: figure out how to make react.js get functions
 function ActivityList(props) {
 	return (
-		<Table style={props.style}>
+		<Table>
 			<thead>
 				<tr>
-					<th>Status</th>
-					<th width={'80%'}>Activity</th>
+					<th><center>Status</center></th>
+					<th width={'80%'}><center>Activity</center></th>
 				</tr>
 			</thead>
 			<tbody>
-				{props.generate.map(function(value, iter) {
-					return <ParseActivity activity={value} key={iter}/>;
+				{props.generate.pending.map(function(value, iter) {
+					return <ParseActivity activity={value} status={'pending'} key={iter} />;
+				})}
+				{props.generate.confirmed.map(function(value, iter) {
+					return <ParseActivity activity={value} status={'confirmed'} key={iter} />
 				})}
 			</tbody>
 		</Table>
 	);
 }
 
-
-
 class Activity extends React.Component {
+
     //The constructor for activity page.
     //Accepts properties:
     //  user - The user this activity page is for
+	//may be changed according to the specification.
     constructor(props) {
         super(props)
 
         //The list of all activities that can be associated with the user provided
-        this.activities = {
-            pending: this.getPendingActivities(props.user),
-            history: this.getHistoryOfActivities(props.user),
-            every: this.getEveryActivities()
-        };
+        this.activities = this.getActivities('')
     }
 
     //returns every pending activities of the user
-    getPendingActivities(user) {
+    getActivities(user) {
         //currently returns a temporary list that represents the pending activities of the user
-        return [
-            'tempact1', 'tempact2', 'tempact3', 'tempact4'
-        ];
-    }
-
-    //returns every activities done by the user
-    getHistoryOfActivities(user) {
-        //currently returns a temporary list that represents all activities done by user
-        return [
-            'tempact1', 'tempact2', 'tempact3', 'tempact4', 'tempact5'
-        ];
+        return {
+            pending: ['tempact1', 'tempact2', 'tempact3', 'tempact4'],
+			confirmed: ['tempact5', 'tempact6']
+        };
     }
 
     //returns list of all activities stored in the database
-    //may be moved to a more general class as this is too general for the scope of activity.
+    //may be moved to a more general class as this is too general for the activity (should only contain stuff for individual users, not every activities)
     getEveryActivities() {
         //currently returns a temporary list that represents all activities stored in the database
-        return [
-            'tempact1', 'tempact2', 'tempact3', 'tempact4', 'tempact5', 'tempact6', 'tempact7', 'tempact9'
-        ];
+        return {
+        	pending: ['tempact1', 'tempact2', 'tempact3', 'tempact4'],
+			confirmed: ['tempact5', 'tempact6', 'tempact7', 'tempact9']
+        };
     }
 
     //The render function for students
     renderStudent() {
         return (
             <div style={{margin: 'auto'}}>
-                <h2>This is the activity page for students</h2>
-                <ActivityList generate={this.activities.pending} style={{margin: 'auto'}}/>
+                <ActivityList generate={this.activities} />
             </div>
         );
     }
 
     //The render function that gets called whenever this page is loaded
     render() {
+		//Only provides student's page for now
         return this.renderStudent();
     }
 }
